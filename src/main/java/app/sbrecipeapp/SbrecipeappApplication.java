2da12cb4a0e032b0afcf0f538f449b038b66d3e6
MrMadWolf <mdshoykotislam05@gmail.com>
2022-08-30 19:30:37 +0600

    first commit

---
+++ src/main/java/app/sbrecipeapp/SbrecipeappApplication.java
@@ -0,0 +1,13 @@
+package app.sbrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbrecipeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/sbrecipeapp/SbrecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sbrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrecipeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
