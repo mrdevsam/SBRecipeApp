2b44a9093d268eaae82b4c9bd9bd9a7f67771423
mrmadwolf <mdshoykotislam05@gmail.com>
2022-09-01 14:05:01 +0000

    jpa query methods added. Closes #8

--- src/main/java/app/sbrecipeapp/controllers/IndexController.java
+++ src/main/java/app/sbrecipeapp/controllers/IndexController.java
@@ -1,13 +1,36 @@
 package app.sbrecipeapp.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import app.sbrecipeapp.domain.Category;
+import app.sbrecipeapp.domain.UnitOfMeasure;
+import app.sbrecipeapp.repositories.CategoryRepository;
+import app.sbrecipeapp.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
     
+    //constructor based dependency injection
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"" , "/" , "/index" , "index.html"})
     public String indexController() {
+
+        Optional<Category> cOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category id :" + cOptional.get().getId());
+        System.out.println("UOM id : " + uOptional.get().getId());
+        
         return "index.html";
     }
 }
--- src/main/java/app/sbrecipeapp/repositories/RecipeRepository.java
+++ src/main/java/app/sbrecipeapp/repositories/RecipeRepository.java
@@ -1,9 +1,11 @@
 package app.sbrecipeapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import app.sbrecipeapp.domain.Recipe;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long>{
-    
+    Optional<Recipe> findByDescription(String description);
 }
