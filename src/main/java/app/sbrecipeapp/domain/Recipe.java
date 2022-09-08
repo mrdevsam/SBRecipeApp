ddfeeffcd05a321138d4f99ac5d5264a3ff88b60
mrmadwolf <mdshoykotislam05@gmail.com>
2022-09-08 18:52:19 +0000

    updated files. #12

--- src/main/java/app/sbrecipeapp/domain/Recipe.java
+++ src/main/java/app/sbrecipeapp/domain/Recipe.java
@@ -58,82 +58,6 @@ public class Recipe {
 	private Set<Category> categories = new HashSet<>();//here, "recipe_category" is the table name, "recipe_id" is the column name
 	//provided by Recipe entity. "category_id" is the column name will be provided by the Category entity.
 
-/* 	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getSource() {
-		return source;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getDirections() {
-		return directions;
-	}
-
-	public void setDirections(String directions) {
-		this.directions = directions;
-	}
-
-	public Integer getPrepTime() {
-		return prepTime;
-	}
-
-	public void setPrepTime(Integer prepTime) {
-		this.prepTime = prepTime;
-	}
-
-	public Integer getCookTime() {
-		return cookTime;
-	}
-
-	public void setCookTime(Integer cookTime) {
-		this.cookTime = cookTime;
-	}
-
-	public Integer getServings() {
-		return servings;
-	}
-
-	public void setServings(Integer servings) {
-		this.servings = servings;
-	}
-
-	public Byte[] getImage() {
-		return image;
-	}
-
-	public void setImage(Byte[] image) {
-		this.image = image;
-	}
-
-	public Notes getNotes() {
-		return notes;
-	} */
-
 	public void setNotes(Notes notes) {
 		this.notes = notes;
 		notes.setRecipe(this);
@@ -144,28 +68,4 @@ public class Recipe {
 		this.ingredients.add(ingredient);
 		return this;
 	}
-
-/* 	public Set<Ingredient> getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(Set<Ingredient> ingredients) {
-		this.ingredients = ingredients;
-	}
-
-	public Difficulty getDifficulty() {
-		return difficulty;
-	}
-
-	public void setDifficulty(Difficulty difficulty) {
-		this.difficulty = difficulty;
-	}
-	public Set<Category> getCategories() {
-		return categories;
-	}
-
-	public void setCategories(Set<Category> categories) {
-		this.categories = categories;
-	}
-	 */
 }
