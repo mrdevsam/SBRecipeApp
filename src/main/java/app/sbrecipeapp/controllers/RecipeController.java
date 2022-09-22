5566cee2acc052ca8f81f2fb3a179ebdf9fdca48
mrmadwolf <mdshoykotislam05@gmail.com>
2022-09-22 20:28:53 +0000

    new recipe created. closes #25

--- src/main/java/app/sbrecipeapp/controllers/RecipeController.java
+++ src/main/java/app/sbrecipeapp/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package app.sbrecipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import app.sbrecipeapp.commands.RecipeCommand;
 import app.sbrecipeapp.services.RecipeService;
 
 @Controller
@@ -20,5 +23,17 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", rService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = rService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
