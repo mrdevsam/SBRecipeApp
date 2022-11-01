baae3b20e38b07fecd6aa469280a7d282cf54353
mrdevsam <mdshoykotislam05@gmail.com>
2022-11-01 18:39:07 +0600

    task completed. closes #39

--- src/main/java/app/sbrecipeapp/controllers/RecipeController.java
+++ src/main/java/app/sbrecipeapp/controllers/RecipeController.java
@@ -67,4 +67,15 @@ public class RecipeController {
         modelAndView.addObject("exception", exception);
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNotFoundOnNumberFomat(Exception exception) {
+        log.error("handling not found exception");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
+    }
 }
--- src/test/java/app/sbrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/app/sbrecipeapp/controllers/RecipeControllerTest.java
@@ -109,4 +109,13 @@ public class RecipeControllerTest {
         .andExpect(status().isNotFound())
         .andExpect(view().name("404error"));
     }
+
+    @Test
+    void testGetRecipeNotFoundOnNumberFormat() throws Exception{
+        when(rService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/addfs/show"))
+        .andExpect(status().isBadRequest())
+        .andExpect(view().name("400error"));
+    }
 }
