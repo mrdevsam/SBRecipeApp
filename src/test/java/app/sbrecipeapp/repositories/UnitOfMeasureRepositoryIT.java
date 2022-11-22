2dc476770acae57f2081e1467d58df649d64c96e
mrdevsam <mdshoykotislam05@gmail.com>
2022-11-22 15:29:43 +0000

    one test fixed, two remaining. #51

--- src/test/java/app/sbrecipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/app/sbrecipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,5 +1,6 @@
 package app.sbrecipeapp.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,18 +8,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import app.sbrecipeapp.bootstrap.RecipeBootstrap;
 import app.sbrecipeapp.domain.UnitOfMeasure;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Optional;
-@Disabled
+// @Disabled
 @ExtendWith(SpringExtension.class)
 @DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
     UnitOfMeasureRepository uMeasureRepository;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        categoryRepository.deleteAll();
+        uMeasureRepository.deleteAll();
+        recipeRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, uMeasureRepository);
+
+        recipeBootstrap.onApplicationEvent(null);
+    }
 
     @Test
     void testFindByDescription() throws Exception{
